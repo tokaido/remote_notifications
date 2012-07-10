40f005fefbe0fbc242c8124c1b196b255bddc0c1
tomhuda <tomhuda@tilde.io>
2012-07-10 11:22:10 -0700

    Support Rails 3.0 and 3.1

--- lib/remote_notifications.rb
+++ lib/remote_notifications.rb
@@ -3,6 +3,10 @@ require "active_support/json"
 require "active_support/notifications"
 require "remote_notifications/version"
 
+unless defined?(ActiveSupport::Notifications::Subscribers::Timed)
+  require "remote_notifications/backport"
+end
+
 module ActiveSupport
   module Notifications
     class Event
@@ -12,8 +16,14 @@ module ActiveSupport
         name, start, finish, id, payload =
           object.values_at("name", "time", "end", "transaction_id", "payload")
 
-        start = DateTime.parse(start)
-        finish = DateTime.parse(finish)
+        # Older versions of Rails return a DateTime here
+        if start.is_a?(String)
+          start = DateTime.parse(start)
+          finish = DateTime.parse(finish)
+        else
+          start = start.to_datetime
+          finish = finish.to_datetime
+        end
 
         new(name, start, finish, id, payload)
       end
